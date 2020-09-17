ac141f7e77101f5e731f092017827d6e5a8f5ba2
Brandan Neises <bneises@splunk.com>
2020-09-17 13:51:46 -0500

    resolved conflicts in cef.py and utils.py

--- phantom/utils.py
+++ phantom/utils.py
@@ -1,25 +1,11 @@
 import socket
 
-CONTAINS_VALIDATORS = {
-    # "domain": is_domain,
-    # "ip": is_ip,
-    # "hash": is_hash,
-    "sha1": is_sha1,
-    # "sha256": is_sha256,
-    # "sha512": is_sha512,
-    # "md5": is_md5,
-    # "host name": is_hostname,
-    # "mac address": is_mac,
-    # "url": is_url,
-    # "email": is_email
-}
-
 
 def is_sha1(maybe_sha):
     if len(maybe_sha) != 40:
         return False
     try:
-        sha_int = int(maybe_sha, 16)
+        int(maybe_sha, 16)
     except ValueError:
         return False
     return True
@@ -28,11 +14,10 @@ def is_sha1(maybe_sha):
 def get_list_from_string(list_string):
     return list_string.split(',')
 
+
 def is_ip(maybe_ip):
     try:
         socket.inet_aton(maybe_ip)
         return True
     except socket.error:
         return False
-
-CONTAINS_VALIDATORS={}
