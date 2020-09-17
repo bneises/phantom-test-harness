e7adbd5b67d008d4cf54a4ce49b0dbbb4d579062
Brandan Neises <bneises@splunk.com>
2020-09-17 13:52:51 -0500

    resolved conflicts in cef.py and utils.py

--- phantom/utils.py
+++ phantom/utils.py
@@ -1,19 +1,16 @@
 import socket
 
-
 def is_sha1(maybe_sha):
     if len(maybe_sha) != 40:
         return False
     try:
-        int(maybe_sha, 16)
+        sha_int = int(maybe_sha, 16)
     except ValueError:
         return False
     return True
 
-
 def get_list_from_string(list_string):
     return list_string.split(',')
-
 
 def is_ip(maybe_ip):
     try:
@@ -21,3 +18,5 @@ def is_ip(maybe_ip):
         return True
     except socket.error:
         return False
+
+CONTAINS_VALIDATORS={}
